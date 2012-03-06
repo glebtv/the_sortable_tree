b8d4b8ab29d1ac40acacebbe7f74f65d5be2bcb3
glebtv <glebtv@gmail.com>
2012-03-06 18:58:05 +0400

    Hash syntax

--- app/helpers/the_sortable_tree_helper.rb
+++ app/helpers/the_sortable_tree_helper.rb
@@ -5,9 +5,9 @@ module TheSortableTreeHelper
   # github.com/the-teacher
   #-------------------------------------------------------------------------------------------------------
 
-  # = sortable_tree @pages,    :new_url => new_page_url, :max_levels => 5
-  # = sortable_tree @products, :new_url => new_product_url, :path => 'products/the_sortable_tree'
-  # = sortable_tree @catalogs, :namespace => :admin, :new_url => new_catalog_url, :max_levels => 5
+  # = sortable_tree @pages,    new_url: new_page_url, max_levels: 5
+  # = sortable_tree @products, new_url: new_product_url, path: 'products/the_sortable_tree'
+  # = sortable_tree @catalogs, namespace: :admin, new_url: new_catalog_url, max_levels: 5
 
   def define_class_of_elements_of tree
     case
@@ -19,22 +19,22 @@ module TheSortableTreeHelper
 
   def sortable_tree(tree, opts= {})
     opts.merge!({
-      :path  => opts[:path] || :the_sortable_tree,
-      :klass => define_class_of_elements_of(tree),
-      :title => opts[:title] || :title,
-      :max_levels => opts[:max_levels] || 3,
-      :namespace => Array.wrap(opts[:namespace])
+      path:  opts[:path] || :the_sortable_tree,
+      klass: define_class_of_elements_of(tree),
+      title: opts[:title] || :title,
+      max_levels: opts[:max_levels] || 3,
+      namespace: Array.wrap(opts[:namespace])
     })
-    render :partial => "#{opts[:path]}/tree", :locals => { :tree => sortable_tree_builder(tree, opts), :opts => opts }
+    render partial: "#{opts[:path]}/tree", locals: { tree: sortable_tree_builder(tree, opts), opts: opts }
   end
 
   def sortable_tree_builder(tree, options= {})
     result = ''
     opts = {
-      :id => :id,           # node id field
-      :node => nil,         # node
-      :root => false,       # is it root node?
-      :level => 0           # recursion level
+      id: :id,           # node id field
+      node: nil,         # node
+      root: false,       # is it root node?
+      level: 0           # recursion level
     }.merge!(options)
 
     root = opts[:root]
@@ -43,18 +43,18 @@ module TheSortableTreeHelper
     unless node
       roots = tree.select{|elem| elem.parent_id.nil?}
       roots.each do |root|
-        _opts = opts.merge({:node => root, :root => true, :level => opts[:level].next})
+        _opts = opts.merge({node: root, root: true, level: opts[:level].next})
         result << sortable_tree_builder(tree, _opts)
       end
     else
       children_res = ''
       children = tree.select{|elem| elem.parent_id == node.id}
-      opts.merge!({:has_children => children.blank?})
+      opts.merge!({has_children: children.blank?})
       children.each do |elem|
-        _opts = opts.merge({:node => elem, :root => false, :level => opts[:level].next})
+        _opts = opts.merge({node: elem, root: false, level: opts[:level].next})
         children_res << sortable_tree_builder(tree, _opts)
       end
-      result << render(:partial => "#{opts[:path]}/node", :locals => {:opts => opts, :root => root, :node => node, :children => children_res})
+      result << render(partial: "#{opts[:path]}/node", locals: {opts: opts, root: root, node: node, children: children_res})
     end
     raw result
   end# sortable_tree_builder
--- lib/the_sortable_tree.rb
+++ lib/the_sortable_tree.rb
@@ -11,8 +11,8 @@ module TheSortableTree
     def self.included(base)
       if defined?(Mongoid)
         base.class_eval do
-          scope :nested_set,          order_by(:lft => :asc)
-          scope :reversed_nested_set, order_by(:lft => :desc)
+          scope :nested_set,          order_by(lft: :asc)
+          scope :reversed_nested_set, order_by(lft: :desc)
         end
       else
         base.class_eval do
